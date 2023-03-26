508c9129640287fec9e9943ba30e1892e131969b
Gudvine <karapuz719@gmail.com>
2023-03-26 20:15:51 +0300

    Added initial migrations

---
+++ DigitalGraduate/Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DigitalGraduate.Data.Models.Catalogs;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalGraduate.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public virtual DbSet<TrainingArea> TrainingAreas { get; set; }
+    }
+}
---
+++ DigitalGraduate/Data/Models/Catalogs/ScientificSpeciality.cs
@@ -0,0 +1,11 @@
+﻿namespace DigitalGraduate.Data.Models.Catalogs
+{
+    /// <summary>
+    /// Представляет справочник "Научные специальности"
+    /// </summary>
+    public class ScientificSpeciality
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } 
+    }
+}
