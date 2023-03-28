5a87b70c213e8ee8441457baeec6c92e2bff9070
Gudvine <karapuz719@gmail.com>
2023-03-28 23:30:56 +0300

    Добавлены новые справочники

---
+++ DigitalGraduate/Data/Models/Catalogs/University.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalGraduate.Data.Models.Catalogs
+{
+    /// <summary>
+    /// Представляет справочник "Учебные заведения (вузы)"
+    /// </summary>
+    public class University
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ShortName { get; set; }
+        public bool IsRegion { get; set; }
+    }
+}
---
+++ DigitalGraduate/Data/Models/Catalogs/UniversityEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DigitalGraduate.Data.Models.Catalogs
+{
+    /// <summary>
+    /// Представляет справочник "Мероприятия вуза"
+    /// </summary>
+    public class UniversityEvent
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Score { get; set; }
+    }
+}
