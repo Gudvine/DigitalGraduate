9c86a296f3258bf62c3233b55ba2474af47eb45f
Gudvine <karapuz719@gmail.com>
2026-04-15 21:29:06 +0300

    Автомиграции

--- DigitalGraduate/Program.cs
+++ DigitalGraduate/Program.cs
@@ -146,6 +146,12 @@ if (app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 //app.UseHttpsRedirection();
 
 app.UseCors("CorsForTests");
